6aba9f4657d154512fc65d2349475dec28010650
Rohan Sakhare <126242412+rohsak5649@users.noreply.github.com>
2024-02-29 21:58:29 +0530

    Create listelementdelete.swift

---
+++ listelementdelete.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  ListView
+//
+//  Created by Rohan Sakhare on 29/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var Fruits : [String] = ["Mango","Banana","Apple","Watermelon","Grapes"]
+    var body: some View {
+        VStack{
+            List{
+                Section(header: Text("Fruiys")) {
+                    ForEach(Fruits , id: \.self ){ fruit in
+                        Text(fruit)
+                    }
+                    .onDelete(perform: delete)
+                }
+            }
+        }
+    }
+    func delete (indexSet : IndexSet){
+        Fruits.remove(atOffsets: indexSet)
+    }
+}
+
+#Preview {
+    ContentView()
+}
